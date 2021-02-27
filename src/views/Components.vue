0332e9135c3daaf5fee32ce8ace882288c4c5583
baylib <benjamin.jonard@prontonmail.com>
2021-02-27 17:56:12 +0100

    first components list

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -3,9 +3,13 @@
     <h1>Form</h1>
     <section>
       <h2>text input</h2>
-      <div></div>
+      <div class="my-5">
+        <input type="text" name="first_name" id="first_name" />
+      </div>
       <h2>label</h2>
-      <div></div>
+      <div>
+        <label for="first_name">First Name</label>
+      </div>
       <h2>bouton</h2>
       <div class="flex justify-between my-5">
         <div class="flex flex-col items-center">
@@ -18,11 +22,28 @@
           <button-ternary>Ternary</button-ternary>
         </div>
       </div>
-      <h2>check list</h2>
-      <div></div>
+      <h2>Text Area</h2>
+      <div class="my-5">
+        <textarea
+          class="form-textarea mt-1 block"
+          placeholder="Enter some text..."
+        ></textarea>
+      </div>
+      <h2>Select</h2>
+      <div class="my-5">
+        <select class="form-select block  mt-1">
+          <option>Option 1</option>
+          <option>Option 2</option>
+        </select>
+      </div>
+      <h2>Check Box</h2>
+      <div>
+        <label class="inline-flex items-center">
+        <input type="checkbox" class="form-checkbox" checked>
+        <span class="ml-2">Option 1</span>
+        </label>
+      </div>
     </section>
-    <h1>Form</h1>
-    <section></section>
   </div>
 </template>
 <script>
